53fcd70089c2e24a12874073a3a3b3ca70967293
gilpgnet <gilpgnet@gmail.com>
2018-12-06 11:28:27 -0600

    Funiona en Internet

--- servicios/conecta.php
+++ servicios/conecta.php
@@ -1,5 +1,4 @@
 <?php
-require_once("conexion.php");
+require_once("Conexion.php");
 $mysqli = new mysqli("localhost", "ususeg", "ususeg", "ususeg");
-//$mysqli = new mysqli("localhost", "id5478262_ususeg", "id5478262_ususeg", "segundo");
 $con = new Conexion($mysqli);
